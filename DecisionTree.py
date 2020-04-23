0a32dfd673b1a28afa9fa07e6f2516b5dbc2f82a
Amratya Saraswat <amratyasaraswat@gmail.com>
2020-04-23 18:29:18 -0500

    :Fire: completed AdaBoost partially

--- DecisionTree.py
+++ DecisionTree.py
@@ -72,108 +72,118 @@ for index in range(len(trainFeatures)):
 
 ####################################################################################
 
-# 1(a) Decision Tree classifier
-cv_score = []
-max_leaf_nodes = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
+# # 1(a) Decision Tree classifier
+# cv_score = []
+# max_leaf_nodes = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
 
-for x in (max_leaf_nodes):
-    model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = x)
-    predict_score = cross_val_score(model, simpleTrain, trainDigits, cv= 10)
-    cv_score.append(1 - predict_score.mean())
+# for x in (max_leaf_nodes):
+#     model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = x)
+#     predict_score = cross_val_score(model, simpleTrain, trainDigits, cv= 10)
+#     cv_score.append(1 - predict_score.mean())
 
-mp.title("Figure 4.1 Cross validation Error for Decision Tree with different max_leaf_node")
-mp.plot(max_leaf_nodes, cv_score)
-mp.xscale("log")
-mp.show()
-
-
-# 1(b) Decision Tree classifier
-
-# ?  Looking at the graph we can say that as the value of the max_leaf_node
+# mp.title("Figure 4.1 Cross validation Error for Decision Tree with different max_leaf_node")
+# mp.plot(max_leaf_nodes, cv_score)
+# mp.xscale("log")
+# mp.show()
 
 
-# 1(c) Decision Tree classifier
+# # 1(b) Decision Tree classifier
 
-# ? The optimal model with minimum cross validation error is with max_leaf_nodes = 40, 500 amd 10000 
-# ? but with max_leaf_nodes = 500, 10000 the model gets overfit. Hence when max_leaf_nodes = 40 the models has lowest cross validation error
+# # ?  Looking at the graph we can say that as the value of the max_leaf_node
 
-xPred = []
-yPred = []
-cPred = []
 
-model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = 40)
-model = model.fit(simpleTrain, trainDigits) 
+# # 1(c) Decision Tree classifier
 
-# Use input points to get predictions here
-for xP in range(-100,100):
-    xP = xP/100.0
-    for yP in range(-100,100):
-        yP = yP/100.0
-        xPred.append(xP)
-        yPred.append(yP)
-        if(model.predict([[xP,yP]])=="1.0"):
-            cPred.append("b")
-        else:
-            cPred.append("r")
+# # ? The optimal model with minimum cross validation error is with max_leaf_nodes = 40, 500 amd 10000 
+# # ? but with max_leaf_nodes = 500, 10000 the model gets overfit. Hence when max_leaf_nodes = 40 the models has lowest cross validation error
 
-## Visualize Results
-#plot the points
-mp.scatter(X,Y,s=3,c=colors)
+# xPred = []
+# yPred = []
+# cPred = []
 
-#plot the regions
-mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
+# model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = 40)
+# model = model.fit(simpleTrain, trainDigits) 
 
-#setup the axes
-mp.xlim(-1,1)
-mp.ylim(-1,1)
-mp.title("Figure 4.2 Decision Tree with minimum Cross Validation Error")
-show()
+# # Use input points to get predictions here
+# for xP in range(-100,100):
+#     xP = xP/100.0
+#     for yP in range(-100,100):
+#         yP = yP/100.0
+#         xPred.append(xP)
+#         yPred.append(yP)
+#         if(model.predict([[xP,yP]])=="1.0"):
+#             cPred.append("b")
+#         else:
+#             cPred.append("r")
 
-# 1(e) Random Forest Classifier
+# #plot the points
+# mp.scatter(X,Y,s=3,c=colors)
 
-rf_max_leaf_nodes = [10,100,1000]
-rf_n_estimator = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
+# #plot the regions
+# mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
 
-cvs_rf_first =[] 
-cvs_rf_second = []
-cvs_rf_third = []
+# #setup the axes
+# mp.xlim(-1,1)
+# mp.ylim(-1,1)
+# mp.title("Figure 4.2 Decision Tree with minimum Cross Validation Error")
+# show()
 
-# ! Cross validation Error for Random Forest with max_leaf_node = 10 and different n_estimators
-for x in (rf_n_estimator):
-    rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 10)
-    predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
-    cvs_rf_first.append(1 - predict_rf_score.mean())
+# # 1(e) Random Forest Classifier
 
-mp.title("Figure 4.3 Cross validation Error for Random Forest with max_leaf_node = 10")
-mp.plot(rf_n_estimator, cvs_rf_first)
+# rf_max_leaf_nodes = [10,100,1000]
+# rf_n_estimator = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
+
+# cvs_rf_first =[] 
+# cvs_rf_second = []
+# cvs_rf_third = []
+
+# # ! Cross validation Error for Random Forest with max_leaf_node = 10 and different n_estimators
+# for x in (rf_n_estimator):
+#     rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 10)
+#     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
+#     cvs_rf_first.append(1 - predict_rf_score.mean())
+
+# mp.title("Figure 4.3 Cross validation Error for Random Forest with max_leaf_node = 10")
+# mp.plot(rf_n_estimator, cvs_rf_first)
+# mp.xscale("log")
+# mp.show() 
+
+# # ! Cross validation Error for Random Forest with max_leaf_node = 100 and different n_estimators
+# for x in (rf_n_estimator):
+#     rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 100)
+#     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
+#     cvs_rf_second.append(1 - predict_rf_score.mean())
+
+# mp.title("Figure 4.4 Cross validation Error for Random Forest with max_leaf_node = 100")
+# mp.plot(rf_n_estimator, cvs_rf_second)
+# mp.xscale("log")
+# mp.show() 
+
+# # ! Cross validation Error for Random Forest with max_leaf_node = 1000 and different n_estimators
+# for x in (rf_n_estimator):
+#     rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 1000)
+#     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
+#     cvs_rf_third.append(1 - predict_rf_score.mean())
+
+# mp.title("Figure 4.5 Cross validation Error for Random Forest with max_leaf_node = 100")
+# mp.plot(rf_n_estimator, cvs_rf_third)
+# mp.xscale("log")
+# mp.show() 
+
+# 1(j) AdaBoost Classifier
+
+adab_n_estimator = [1,5,10,100,1000,10000]
+cvs_adab = []
+
+for x in adab_n_estimator:
+    adab_model = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=1, criterion='entropy'), n_estimators = x)
+    predict_adab_score = cross_val_score(adab_model, simpleTrain, trainDigits, cv=10)
+    cvs_adab.append(1 - predict_adab_score.mean())
+
+mp.title("Figure 4.7 Cross validation Error for AdaBoost with different n_estimator")
+mp.plot(adab_n_estimator, cvs_adab)
 mp.xscale("log")
 mp.show() 
-
-# ! Cross validation Error for Random Forest with max_leaf_node = 100 and different n_estimators
-for x in (rf_n_estimator):
-    rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 100)
-    predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
-    cvs_rf_second.append(1 - predict_rf_score.mean())
-
-mp.title("Figure 4.4 Cross validation Error for Random Forest with max_leaf_node = 100")
-mp.plot(rf_n_estimator, cvs_rf_second)
-mp.xscale("log")
-mp.show() 
-
-# ! Cross validation Error for Random Forest with max_leaf_node = 1000 and different n_estimators
-for x in (rf_n_estimator):
-    rf_model = RandomForestClassifier(n_estimators = x, criterion = 'entropy', max_leaf_nodes = 1000)
-    predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits)
-    cvs_rf_third.append(1 - predict_rf_score.mean())
-
-mp.title("Figure 4.5 Cross validation Error for Random Forest with max_leaf_node = 100")
-mp.plot(rf_n_estimator, cvs_rf_third)
-mp.xscale("log")
-mp.show() 
-
-
-
-
 
 
 
