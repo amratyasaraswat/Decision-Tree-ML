2c252ce6fa6f11cdfe613b7ceb777850773b618a
Amratya Saraswat <amratyasaraswat@gmail.com>
2020-04-23 19:35:06 -0500

    :fire: Partially done with Part 1

--- DecisionTree.py
+++ DecisionTree.py
@@ -72,63 +72,63 @@ for index in range(len(trainFeatures)):
 
 ####################################################################################
 
-# # 1(a) Decision Tree classifier
-# cv_score = []
-# max_leaf_nodes = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
+# 1(a) Decision Tree classifier
+cv_score = []
+max_leaf_nodes = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
 
-# for x in (max_leaf_nodes):
-#     model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = x)
-#     predict_score = cross_val_score(model, simpleTrain, trainDigits, cv= 10)
-#     cv_score.append(1 - predict_score.mean())
+for x in (max_leaf_nodes):
+    model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = x)
+    predict_score = cross_val_score(model, simpleTrain, trainDigits, cv= 10)
+    cv_score.append(1 - predict_score.mean())
 
-# mp.title("Figure 4.1 Cross validation Error for Decision Tree with different max_leaf_node")
-# mp.plot(max_leaf_nodes, cv_score)
-# mp.xscale("log")
-# mp.show()
-
-
-# # 1(b) Decision Tree classifier
-
-# # ?  Looking at the graph we can say that as the value of the max_leaf_node
+mp.title("Figure 4.1 CV Error for Decision Tree with different max_leaf_node")
+mp.plot(max_leaf_nodes, cv_score)
+mp.xscale("log")
+mp.show()
 
 
-# # 1(c) Decision Tree classifier
+# 1(b) Decision Tree classifier
 
-# # ? The optimal model with minimum cross validation error is with max_leaf_nodes = 40, 500 amd 10000 
-# # ? but with max_leaf_nodes = 500, 10000 the model gets overfit. Hence when max_leaf_nodes = 40 the models has lowest cross validation error
+# ?  Looking at the graph we can say that as the value of the max_leaf_node
 
-# xPred = []
-# yPred = []
-# cPred = []
 
-# model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = 40)
-# model = model.fit(simpleTrain, trainDigits) 
+# 1(c) Decision Tree classifier
 
-# # Use input points to get predictions here
-# for xP in range(-100,100):
-#     xP = xP/100.0
-#     for yP in range(-100,100):
-#         yP = yP/100.0
-#         xPred.append(xP)
-#         yPred.append(yP)
-#         if(model.predict([[xP,yP]])=="1.0"):
-#             cPred.append("b")
-#         else:
-#             cPred.append("r")
+# ? The optimal model with minimum cross validation error is with max_leaf_nodes = 40, 500 amd 10000 
+# ? but with max_leaf_nodes = 500, 10000 the model gets overfit. Hence when max_leaf_nodes = 40 the models has lowest cross validation error
 
-# #plot the points
-# mp.scatter(X,Y,s=3,c=colors)
+xPred = []
+yPred = []
+cPred = []
 
-# #plot the regions
-# mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
+model = DecisionTreeClassifier(criterion = 'entropy', max_leaf_nodes = 40)
+model = model.fit(simpleTrain, trainDigits) 
 
-# #setup the axes
-# mp.xlim(-1,1)
-# mp.ylim(-1,1)
-# mp.title("Figure 4.2 Decision Tree with minimum Cross Validation Error")
-# show()
+# Use input points to get predictions here
+for xP in range(-100,100):
+    xP = xP/100.0
+    for yP in range(-100,100):
+        yP = yP/100.0
+        xPred.append(xP)
+        yPred.append(yP)
+        if(model.predict([[xP,yP]])=="1.0"):
+            cPred.append("b")
+        else:
+            cPred.append("r")
 
-# # 1(e) Random Forest Classifier
+#plot the points
+mp.scatter(X,Y,s=3,c=colors)
+
+#plot the regions
+mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
+
+#setup the axes
+mp.xlim(-1,1)
+mp.ylim(-1,1)
+mp.title("Figure 4.2 Decision Region with minimum CV Error")
+show()
+
+# 1(e) Random Forest Classifier
 
 # rf_max_leaf_nodes = [10,100,1000]
 # rf_n_estimator = [5,10,15,20,30,40,50,75,100,200,500,1000,10000]
@@ -143,7 +143,7 @@ for index in range(len(trainFeatures)):
 #     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
 #     cvs_rf_first.append(1 - predict_rf_score.mean())
 
-# mp.title("Figure 4.3 Cross validation Error for Random Forest with max_leaf_node = 10")
+# mp.title("Figure 4.3 CV Error for Random Forest with max_leaf_node = 10")
 # mp.plot(rf_n_estimator, cvs_rf_first)
 # mp.xscale("log")
 # mp.show() 
@@ -154,7 +154,7 @@ for index in range(len(trainFeatures)):
 #     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
 #     cvs_rf_second.append(1 - predict_rf_score.mean())
 
-# mp.title("Figure 4.4 Cross validation Error for Random Forest with max_leaf_node = 100")
+# mp.title("Figure 4.4 CVError for Random Forest with max_leaf_node = 100")
 # mp.plot(rf_n_estimator, cvs_rf_second)
 # mp.xscale("log")
 # mp.show() 
@@ -165,59 +165,85 @@ for index in range(len(trainFeatures)):
 #     predict_rf_score = cross_val_score(rf_model, simpleTrain, trainDigits, cv=10)
 #     cvs_rf_third.append(1 - predict_rf_score.mean())
 
-# mp.title("Figure 4.5 Cross validation Error for Random Forest with max_leaf_node = 100")
+# mp.title("Figure 4.5 CV Error for Random Forest with max_leaf_node = 100")
 # mp.plot(rf_n_estimator, cvs_rf_third)
 # mp.xscale("log")
 # mp.show() 
 
-# 1(j) AdaBoost Classifier
+# # 1(h)
 
-adab_n_estimator = [1,5,10,100,1000,10000]
-cvs_adab = []
+# # 1(j) AdaBoost Classifier
 
-for x in adab_n_estimator:
-    adab_model = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=1, criterion='entropy'), n_estimators = x)
-    predict_adab_score = cross_val_score(adab_model, simpleTrain, trainDigits, cv=10)
-    cvs_adab.append(1 - predict_adab_score.mean())
+# adab_n_estimator = [1,5,10,100,1000,10000]
+# cvs_adab = []
 
-mp.title("Figure 4.7 Cross validation Error for AdaBoost with different n_estimator")
-mp.plot(adab_n_estimator, cvs_adab)
-mp.xscale("log")
-mp.show() 
+# for x in adab_n_estimator:
+#     adab_model = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=1, criterion='entropy'), n_estimators = x)
+#     predict_adab_score = cross_val_score(adab_model, simpleTrain, trainDigits, cv=10)
+#     cvs_adab.append(1 - predict_adab_score.mean())
 
+# mp.title("Figure 4.7 CV Error for AdaBoost max depth =1")
+# mp.plot(adab_n_estimator, cvs_adab)
+# mp.xscale("log")
+# mp.show() 
 
+# # 1(k) AdaBoost Classifier
 
-#####################################################################################
-# xPred = []
-# yPred = []
-# cPred = []
-# # Use input points to get predictions here
-# for xP in range(-100,100):
-#     xP = xP/100.0
-#     for yP in range(-100,100):
-#         yP = yP/100.0
-#         xPred.append(xP)
-#         yPred.append(yP)
-#         if(model.predict([[xP,yP]])=="1.0"):
-#             cPred.append("b")
-#         else:
-#             cPred.append("r")
+# cvs_adab2 = []
+# for x in adab_n_estimator:
+#     adab_model2 = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=10, criterion='entropy'), n_estimators = x)
+#     predict_adab_score2 = cross_val_score(adab_model2, simpleTrain, trainDigits, cv=10)
+#     cvs_adab2.append(1 - predict_adab_score2.mean())
 
-# ## Visualize Results
-# #plot the points
-# mp.scatter(X,Y,s=3,c=colors)
+# mp.title("Figure 4.8 CV Error for AdaBoost with max depth =10")
+# mp.plot(adab_n_estimator, cvs_adab2)
+# mp.xscale("log")
+# mp.show() 
 
-# #plot the regions
-# mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
+# # 1(l) AdaBoost Classifier
 
-# #setup the axes
-# mp.xlim(-1,1)
-# mp.xlabel("Average Intensity")
-# mp.ylim(-1,1)
-# mp.ylabel("Intensity Variance")
-# mp.title("Figure 1.4 Decision Region for 256D")
-# show()
+# cvs_adab3 = []
+# for x in adab_n_estimator:
+#     adab_model3 = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=1000, criterion='entropy'), n_estimators = x)
+#     predict_adab_score3 = cross_val_score(adab_model3, simpleTrain, trainDigits, cv=10)
+#     cvs_adab3.append(1 - predict_adab_score3.mean())
 
+# mp.title("Figure 4.9 CV Error for AdaBoost with max depth =1000")
+# mp.plot(adab_n_estimator, cvs_adab3)
+# mp.xscale("log")
+# mp.show() 
 
+# 1(m) Decision region for AdaBoost Classifier
+
+xPred = []
+yPred = []
+cPred = []
+
+decision_model = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=1, criterion='entropy'), n_estimators = 1000)
+decision_model = model.fit(simpleTrain, trainDigits) 
+
+# Use input points to get predictions here
+for xP in range(-100,100):
+    xP = xP/100.0
+    for yP in range(-100,100):
+        yP = yP/100.0
+        xPred.append(xP)
+        yPred.append(yP)
+        if(decision_model.predict([[xP,yP]])=="1.0"):
+            cPred.append("b")
+        else:
+            cPred.append("r")
+
+#plot the points
+mp.scatter(X,Y,s=3,c=colors)
+
+#plot the regions
+mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.1)
+
+#setup the axes
+mp.xlim(-1,1)
+mp.ylim(-1,1)
+mp.title("Figure 4.10 Decision Region for AdaBoost with min CV Error")
+show()
 
 
